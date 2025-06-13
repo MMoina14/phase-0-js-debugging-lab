9718e79c396c86b60f91130e1dd271c57cd1039b
MMoina14 <miguelamoina@gmail.com>
2025-06-12 20:48:53 -0400

    mydiscount

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,14 @@
-function calculateDiscountedPrice(quantity, pricePerItem) {
-    let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
-        totalPrice += pricePerItem;
-    }
-
-    if (quantity >= 10) {
-        totalPrice *= 0.9;
-    }
-
-    return totalPrice;
-}
 
 
 
 module.exports = calculateDiscountedPrice;
+
+function calculateDiscountedPrice(quantity, pricePerItem) {
+    let totalPrice = quantity * pricePerItem;
+
+    if (quantity >= 10) {
+        totalPrice *= 0.9; 
+    }
+
+    return totalPrice;
+}
